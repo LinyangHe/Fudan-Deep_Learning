4f504d8f58f4ce98f192f66a56e07e7836cb37b5
Linyang He <leonlovesjazz@gmail.com>
2018-10-30 02:21:14 -0500

    Update

--- Project 1/Code/model.py
+++ Project 1/Code/model.py
@@ -12,6 +12,9 @@ class NN(nn.Module):
         self.hidden_1 = nn.Linear(n_feature, n_hidden_1)
         self.hidden_2 = nn.Linear(n_hidden_1, n_hidden_2)
         self.output = nn.Linear(n_hidden_2, n_output)
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.5)
+        self.loss_function = torch.nn.MSELoss()
+
 
     def forward(self, x):
         x = F.relu(self.hidden_1(x))
@@ -19,14 +22,27 @@ class NN(nn.Module):
         x = self.output(x)
         return x
 
-mynet = NN(1, 5, 5, 1)
-
-print(mynet)
+    def train(self, x, epochs):
+        for i in range(epochs):
+            prediction = self.forward(x)
+            loss = self.loss_function(prediction, y)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+        return prediction
+    
 
 def data_create():
-    x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
-    y = x.pow(2) + 0.2*torch.rand(x.size())
+    x = torch.unsqueeze(torch.linspace(-1, 1, 100), 1)
+    #the unsqueeze function turn the vector into a matrix. 
+    y = 2*x.pow(2) + x + 0.2*torch.rand(x.size())
     x, y = Variable(x, requires_grad=False), Variable(y, requires_grad=False)
     return x, y
 
+
+mynet = NN(1, 10, 5, 1)
+
+print(mynet)
+
 x, y = data_create()
+prediction = mynet.train(x, 1000)
