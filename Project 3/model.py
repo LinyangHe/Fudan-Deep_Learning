64c4a79921d20983c626b7fa0f1c186d190be0c4
Linyang He <leonlovesjazz@gmail.com>
2018-12-08 16:27:45 -0600

    Update

--- Project 3/model.py
+++ Project 3/model.py
@@ -4,8 +4,14 @@ import torch
 class Pointer_network(nn.Module):
     def __init__(self, ):
         super().__init__()
-        self.encoder = nn.LSTm
-        self.decoder = 
+        self.encoder = nn.Sequential(
+            nn.LSTM()
+            nn.
+
+        )
+        self.decoder = nn.Seuential(
+
+        )
     
     def forward(self, x):
         encoded = self.encoder(x)
